c74c65e4e27cd75d810366af9d69505f45b31dd6
hossein <ho3eintry@gmail.com>
2021-08-10 16:38:54 +0430

    Add Controller,router and startup

---
+++ controllers/user/user_controller.go
@@ -0,0 +1,10 @@
+package user_controller
+
+import "github.com/gin-gonic/gin"
+
+func Add(c *gin.Context)    {}
+func Update(c *gin.Context) {}
+func Delete(c *gin.Context) {}
+func Get(c *gin.Context)    {}
+func Search(c *gin.Context) {}
+func Login(c *gin.Context)  {}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ho3einTry/bookstore_users-api/app"
+
+func main() {
+	app.StartApplication()
+}
